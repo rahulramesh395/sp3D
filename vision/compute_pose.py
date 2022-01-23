3026a3801010744409d9763196e2312d47196a0c
rahulramesh395 <rahulramesh395@gmail.com>
2022-01-23 15:51:09 +0100

    script to calculate end effector pose

---
+++ vision/compute_pose.py
@@ -0,0 +1,23 @@
+import math
+
+class ComputePose:
+
+	def __init__(vision):
+		self.vision = vision
+		self.pose = None
+
+		#define thresholds
+		z_threahold = None
+		y_threshold = None
+		x_threshold = None
+
+	def _get_center():
+		center = self.vision.center_pos
+		radius = self.vision.radius
+
+	def _compute_area():
+		area = math.pi * self.radius**2
+		return area
+
+	def get_position():
+		return xyz
