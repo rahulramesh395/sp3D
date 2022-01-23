362fdac5a42d7c495357b65c20603314abf1bfb8
rahulramesh395 <rahulramesh395@gmail.com>
2022-01-23 12:00:08 +0100

    function to show frames

--- vision/vision.py
+++ vision/vision.py
@@ -1,13 +1,14 @@
-import cv2 as cv
+import cv2
 import imutils
 import numpy as np
 import atexit
 import time
 
 class Vision:
-	def _init_(self):
+	def __init__(self):
 		self.capture = None
 		self.mask = None
+		self.show = True
 
 		atexit.register(self._clean_up)
 
@@ -18,37 +19,41 @@ class Vision:
 
 	def _start_cam(self):
 		# start camera
-		self.capture = cv.VideoCapture(0)
-
-		# wait for camera to stabilize
-		time.sleep(1)
+		self.capture = cv2.VideoCapture(0)
 
 	def _detect_red_color(self):
-		frame = self._read_frame()
+		ret, frame = self.capture.read()
+
 		# Converting the image to hsv
 		# resize the frame, inverted ("vertical flip" w/ 180degrees),
 		# blur it, and convert it to the HSV color space
 		frame = imutils.resize(frame, width=600)
 		frame = imutils.rotate(frame, angle=180)
-		# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-		hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 		# define range of red color in HSV
 		lower_red = np.array([160,50,50])
 		upper_red = np.array([180,255,255])
 
 		# threshold the HSV image using inRange function to get only red colors
-		mask = cv.inRange(hsv, lower_red, upper_red)
-		mask = cv.erode(mask, None, iterations=2)
-		mask = cv.dilate(mask, None, iterations=2)
+		mask = cv2.inRange(hsv, lower_red, upper_red)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
 		self.mask = mask
 
 	def _read_frame(self):
 		_, frame = self.capture.read()
 		return frame
 
+	def _show_frame(self):
+		#cv.imshow("Original", self.frame)
+		cv2.imshow("Mask", self.mask)
+		if cv2.waitKey(1) == 27:
+			self.show = False
+
 	def _draw_contour(self):
-		cnts = cv.findContours(self.mask.copy(),cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+		cnts = cv.findContours(self.mask.copy(),cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 		center = None
 		# only proceed if at least one contour was found
@@ -70,9 +75,10 @@ class Vision:
 
 	def detect_object(self):
 		self._start_cam()
-		while True:
+		while self.show:
 			self._detect_red_color()
-			self._draw_contour()
+			self._show_frame()
+			#self._draw_contour()
 
 	def get_position(self):
 		return xyz 
